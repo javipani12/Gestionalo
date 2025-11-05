b86b1f89725819184f75974b033a4fdd2a154a82
javipani12 <117436698+javipani12@users.noreply.github.com>
2025-11-05 12:19:30 +0100

    Inicio sesión
    Creado el inicio de sesión y los estilos de este

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <title>Gestionalo</title>
+</head>
+<body>
+    <div id="contenedor_sesion">
+        <form id="inicio_sesion">
+            <label for="email">Indique su correo electrónico</label>
+            <input id="email" type="email">
+            <br><br>
+            <label for="contrasenna">Introduzca su contraseña</label>
+            <input type="password" id="contrasenna">
+            <br><br>
+            <input type="button" value="Iniciar Sesión">
+        </form>
+        <br><hr>
+        <label for="registro">¿No tienes cuenta?</label>
+        <input type="button" value="Registrarse" id="registro">
+    </div>
+</body>
+</html>
